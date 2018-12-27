3e06bc617072d668f00e3afb085af6112b178025
Mathieu Maes <mathieu@zimmo.be>
2018-12-27 23:19:01 +0100

    start adding layout

--- themes/default/theme.js
+++ themes/default/theme.js
@@ -1,3 +1,34 @@
-console.log("Default theme is loaded")
+require('./style.scss');
 
-require('./style.styl')
+function toggleClass(el, className) {
+    if (el.classList) {
+        el.classList.toggle(className);
+    } else {
+        var classes = el.className.split(' ');
+        var existingIndex = classes.indexOf(className);
+
+        if (existingIndex >= 0)
+            classes.splice(existingIndex, 1);
+        else
+            classes.push(className);
+
+        el.className = classes.join(' ');
+    }
+}
+function ready(fn) {
+    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+        fn();
+    } else {
+        document.addEventListener('DOMContentLoaded', fn);
+    }
+}
+ready(function() {
+    var btnMenu = document.querySelector("button.hamburger");
+    var mainMenu = document.querySelector("nav.main-menu");
+    btnMenu
+        .addEventListener("click", function() {
+            toggleClass(mainMenu, "__visible");
+            toggleClass(btnMenu, "is-active");
+            toggleClass(document.body, "__menu-visible");
+        });
+});
